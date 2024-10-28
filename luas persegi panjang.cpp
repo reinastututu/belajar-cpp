fe24c2e04aaa7154d48262f6018a73d11b480e52
febrian1592 <119560086+febrian1592@users.noreply.github.com>
2024-10-28 21:21:33 +0700

    Add files via upload

---
+++ luas persegi panjang.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    int panjang;
+    int lebar;
+   
+   std::cout <<"mencari luas persegi panjang";
+   std::cin >> panjang;
+   std::cin >> lebar;
+   
+  int luas_persegipanjang = panjang * lebar;
+ 
+ std::cout << "hasil dari mencari luas persegi panjang adalah:" << luas_persegipanjang;
+}
