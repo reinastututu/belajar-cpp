993f8a03035f7f397fd1ab8a526fd32e8aa7590d
reina <119560086+reinastututu@users.noreply.github.com>
2025-01-15 21:12:40 +0700

    Add files via upload

---
+++ operator assignment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int n = 500;
+while (n>=50){
+    cout <<"lu yahudi" <<endl;
+n -= 2;
+}
+}
---
+++ while.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int n = 50;
+while (n>=50){
+    cout <<"lu yahudi";
+}
+}
